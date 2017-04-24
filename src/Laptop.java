0600e6af6faac5e990bcb9f95c4a46546f2e5ef0
Omer <Omer@DESKTOP-GRID>
2017-04-24 12:26:57 +0930

    Updated Fields

--- src/Laptop.java
+++ src/Laptop.java
@@ -9,17 +9,40 @@
  * @author Omer
  */
 public class Laptop extends Electronic {
-    private String brand;
     private double screenSize;
     private int ram;
     private String operatingSystem;
     
     
     public Laptop(String br, double ss, int ram, int wa, String os, int ty){
-        super(wa, ty);
-        this.brand = br;
+        super(wa, ty, br);
         this.screenSize = ss;
         this.ram = ram;
         this.operatingSystem = os;
     }
+    
+    public double getScreenSize(){
+        return this.screenSize;
+    }
+    
+    public void setScreenSize(double ss){
+        this.screenSize = ss;
+    }
+    
+    public int getRam(){
+        return this.ram;
+    }
+    
+    public void setRam(int ram){
+        this.ram = ram;
+    }
+    
+    public String getOperatingSystem(){
+        return this.operatingSystem;
+    }
+    
+    public void setOperatingSystem(String os){
+        this.operatingSystem = os;
+    }
 }
+
