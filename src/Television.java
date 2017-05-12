ba92c7ee1401d7f03bf561711afd923183077c9b
Omer <Omer@DESKTOP-GRID>
2017-05-12 16:24:35 +0930

    Completed

--- src/Television.java
+++ src/Television.java
@@ -9,24 +9,14 @@
  * @author Omer
  */
 public class Television extends Electronic{
-    private double screenSize;
     private double energyRating;
     private double weight;
     
     
     public Television(String br, double ss, double er, int wa, double we, int ty){
-        super(wa, ty, br);
-        this.screenSize = ss;
+        super(wa, ty, br, ss);
         this.energyRating = er;
         this.weight = we;
-    }
-    
-    public double getScreenSize(){
-        return this.screenSize;
-    }
-    
-    public void setScreenSize(double ss){
-        this.screenSize = ss;
     }
     
     public double getEnergyRating(){
