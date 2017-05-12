3716ca1303036ccb18f00ed09c4f17353aed3d62
Omer <Omer@DESKTOP-GRID>
2017-05-12 17:29:22 +0930

    Completed

--- src/SmartPhone.java
+++ src/SmartPhone.java
@@ -10,19 +10,19 @@
  */
 public class SmartPhone extends Electronic {
     
-    private int storage;
+    private int storageCapacity;
     
     public SmartPhone(String br, double ss, int st, int wa, int ty){
         super(wa, ty, br, ss);
-        this.storage = st;      
+        this.storageCapacity = st;      
     }
     
-    public int getStorage(){
-        return this.storage;
+    public int getStorageCapacity(){
+        return this.storageCapacity;
     }
     
-    public void setStorage(int st){
-        this.storage = st;
+    public void setStorageCapacity(int st){
+        this.storageCapacity = st;
     }
     
     
